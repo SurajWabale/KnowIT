d251ce963152c96f9a1ea0b1b75e70f29f126ca0
SurajWabale <110296520+SurajWabale@users.noreply.github.com>
2023-06-28 14:48:22 +0530

    Add files via upload

---
+++ LoginServer.js
@@ -0,0 +1,24 @@
+var exp = require('express');
+var bp = require('body-parser')
+
+var app = exp();
+app.use(bp.urlencoded({extended: false}))
+
+app.listen(9000, function () {
+    console.log("Server Started");
+})
+
+app.get('/login', function (req, res) {
+    res.sendFile(__dirname + "/login.html");
+})
+
+app.post('/logincheck', function (req, res) {
+    if (req.body.uid == "object" && req.body.pwd == "knowit") 
+        res.send("<h2>Login Done</h2>");
+     else 
+        res.send("<h2>Login failed</h2>");
+    
+});
+app.all('*', function (req, res) {
+    res.send("Invalid URL"); 
+});
