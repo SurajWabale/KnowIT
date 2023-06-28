0e9918c75ef461f442b0e40ca1f0efe499ac77aa
SurajWabale <110296520+SurajWabale@users.noreply.github.com>
2023-06-28 16:36:47 +0530

    LoginServer.js

--- LoginServer.js
+++ LoginServer.js
@@ -1,7 +1,6 @@
 
 var exp = require('express');
 var bp = require('body-parser')
-var path=require("path");
 var app = exp();
 app.use(exp.static('Public'));
 app.use(bp.urlencoded({extended: false}))
@@ -23,29 +22,3 @@ app.post('/logincheck', function (req, res) {
 app.all('*', function (req, res) {
     res.send("Invalid URL"); 
 });
-
-var exp = require('express');
-var bp = require('body-parser')
-
-var app = exp();
-app.use(bp.urlencoded({extended: false}))
-
-app.listen(9000, function () {
-    console.log("Server Started");
-})
-
-app.get('/login', function (req, res) {
-    res.sendFile(__dirname + "/login.html");
-})
-
-app.post('/logincheck', function (req, res) {
-    if (req.body.uid == "object" && req.body.pwd == "knowit") 
-        res.send("<h2>Login Done</h2>");
-     else 
-        res.send("<h2>Login failed</h2>");
-    
-});
-app.all('*', function (req, res) {
-    res.send("Invalid URL"); 
-});
-
